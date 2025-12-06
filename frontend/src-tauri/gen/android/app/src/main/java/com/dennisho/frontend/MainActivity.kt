67d781e1b291b7a9df873bb4e9539ad84f8ec6d4
dennishooo <hkhdennis@gmail.com>
2025-12-06 10:10:53 +0800

    Initial Commit

---
+++ frontend/src-tauri/gen/android/app/src/main/java/com/dennisho/frontend/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dennisho.frontend
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
